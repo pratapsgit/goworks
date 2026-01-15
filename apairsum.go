d9bc781a9862c1479ec6ddd5ee74ad50813e9243
Pratap M <pk1979m@gmail.com>
2026-01-15 21:06:14 +0530

    prints all pair sum for an target without duplicate

---
+++ apairsum.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func aPairWithSum(arr []int, start int, end int, target int) ([][]int, error) {
+	s := start
+	e := end
+
+	var result [][]int
+
+	slices.Sort(arr)
+
+	for s < e {
+		tsum := arr[s] + arr[e]
+
+		if tsum == target {
+			result = append(result, []int{arr[s], arr[e]})
+			s++
+			e--
+
+			for s < end && arr[s] == arr[s-1] {
+				s++
+			}
+
+			for e > 0 && arr[e] == arr[e+1] {
+				e--
+			}
+		} else if tsum > target {
+			e--
+		} else {
+			s++
+		}
+	}
+
+	return result, nil
+}
+
+func main() {
+	arr := []int{2, 5, 1, 4, 6, 8, 7, 9, 2, 5, 1, 4, 6, 3, 1}
+	target := 8
+
+	fmt.Println(arr, " to find target sum ", target)
+	result, err := aPairWithSum(arr, 0, len(arr)-1, target)
+
+	if err == nil {
+		for _, r := range result {
+			fmt.Printf("{%d, %d}\n", r[0], r[1])
+		}
+	}
+}
